fe17e6e8bf662f83bd0215456779b21e1d7262a8
LAAOUINE Abdellah <abdellah.laaouine@stagiairesmns.fr>
2023-09-25 14:14:22 +0200

    Controller

--- src/main/java/fr/mns/erasmusnetwork/gestionUsers/controller/UserController.java
+++ src/main/java/fr/mns/erasmusnetwork/gestionUsers/controller/UserController.java
@@ -4,11 +4,9 @@ import fr.mns.erasmusnetwork.gestionUsers.model.User;
 import fr.mns.erasmusnetwork.gestionUsers.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -22,9 +20,30 @@ public class UserController {
         return ResponseEntity.ok(userService.getAll());
     }
 
+    // à repasser dessus aprés
     @GetMapping("/{id}")
     public ResponseEntity<User> getById(@PathVariable("id") Long id){
         //TODO
-        return null;
+        return ResponseEntity.ok(userService.getById(id));
+        //return null;
     }
+
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") Long id){
+        //userService.delete(id);
+        User us = userService.getById(id);
+        userService.delete(us);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> create(Long id, String firstName, String lastName, Date date, String mail, String passWord){
+        return  ResponseEntity.ok(userService.create(id, firstName, lastName, date, mail, passWord));
+    }
+
+
+
+
+
+
 }
