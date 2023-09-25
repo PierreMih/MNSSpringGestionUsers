b6273a93c2b8a47080887feb9567fc6ef64f4509
LAAOUINE Abdellah <abdellah.laaouine@stagiairesmns.fr>
2023-09-25 13:50:37 +0200

    Model, service,

--- src/main/java/fr/mns/erasmusnetwork/gestionUsers/service/UserService.java
+++ src/main/java/fr/mns/erasmusnetwork/gestionUsers/service/UserService.java
@@ -5,6 +5,7 @@ import fr.mns.erasmusnetwork.gestionUsers.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -14,15 +15,38 @@ public class UserService {
     private UserRepository userRepository;
 
     public List<User> getAll(){
-
-        return null;
+        return userRepository.findAll();
     }
 
     public User save(User user){
-        return null;
+        return userRepository.findById(user.getId()).orElse(null);
     }
 
     public User getById(long id){
         return userRepository.findById(id).orElse(null);
+    }
+
+    //create, delete, update
+
+    public User create(Long id, String firstName, String lastName, Date date, String mail, String passWord){
+        User us = new User(id, firstName, lastName, date, mail, passWord);
+        // à vérifier
+        userRepository.save(us);
+        return  us;
+    }
+
+    public void delete(User user){
+        userRepository.delete(user);
+    }
+
+    public User update(Long id, String firstName, String lastName, Date date, String mail, String passWord){
+        User us = userRepository.findById(id).orElseThrow();
+        us.setFirstName(firstName);
+        us.setLastName(lastName);
+        us.setBirthDate(date);
+        us.setMail(mail);
+        us.setPassWord(passWord);
+        userRepository.save(us);
+        return  us;
     }
 }
